931a63a3795d996adec6f11bae7cf1b347cb83ff
rony <rniumo@gmail.com>
2024-05-23 00:49:02 +0600

    admin and deploy cd

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from accounts import models
 
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('registration', 'session', 'photo', 'is_approved', 'hall')
+    readonly_fields = ('registration', 'is_approved', 'session', 'hall', 'photo')
+    empty_value_display = 'N/A'
+    list_filter = ('is_approved', 'session')
+    @admin.display(description="Photo")
+    def photo(self, obj):
+        return format_html(f'<img src={obj.profile_picture.url} width="{70}" height="{70}" />')
+
+admin.site.site_header = "Clearance Administration"
+admin.site.site_title = "Backend"
+admin.site.index_title = "Clearance Administration"
 
 admin.site.register(models.InviteToken)
 admin.site.register(models.AdminAccount)
-admin.site.register(models.StudentAccount)
+admin.site.register(models.StudentAccount, StudentAdmin)
 admin.site.register(models.StudentPlaceholder)
 admin.site.register(models.StudentNotice)
--- clearance/admin.py
+++ clearance/admin.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from clearance import models
 
+class ClearanceAdmin(admin.ModelAdmin):
+    list_display = ('student', 'is_approved', 'progress', 'added_at')
+    list_filter = ('is_approved', 'progress', 'added_at')
+    list_per_page = 20
+    readonly_fields = ('is_approved', 'progress', 'student')
+    actions_on_top = False
+    date_hierarchy = 'added_at'
 
 admin.site.register(models.Department)
 admin.site.register(models.Lab)
 admin.site.register(models.Session)
-admin.site.register(models.Clearance)
+admin.site.register(models.Clearance, ClearanceAdmin)
 admin.site.register(models.AdministrativeApproval)
 admin.site.register(models.DeptApproval)
 admin.site.register(models.ClerkApproval)
