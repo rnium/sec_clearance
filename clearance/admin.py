f27dd7a6219322025f0ce1dca2991de3528c5d47
rony <rniumo@gmail.com>
2024-05-23 00:56:11 +0600

    batch sized list per page

--- clearance/admin.py
+++ clearance/admin.py
@@ -4,7 +4,7 @@ from clearance import models
 class ClearanceAdmin(admin.ModelAdmin):
     list_display = ('student', 'is_approved', 'progress', 'added_at')
     list_filter = ('is_approved', 'progress', 'added_at')
-    list_per_page = 20
+    list_per_page = 180
     readonly_fields = ('is_approved', 'progress', 'student')
     actions_on_top = False
     date_hierarchy = 'added_at'
